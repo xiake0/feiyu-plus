07990fca5122d2367df6a02c8e178dc935d62fd0
Route-l <15813384515@163.com>
2019-12-17 21:56:30 +0800

    update

---
+++ src/main/java/com/common/SysConstants.java
@@ -0,0 +1,60 @@
+package com.common;
+
+/**
+ * @Author: xiake
+ * @Date: 2019/12/16 23:31
+ * @Description:
+ **/
+public final class SysConstants {
+
+    public static int STATUS_NORMAL = 1;
+
+    public static int STATUS_ERROR = 0;
+
+    public static String PERMISSION_OPERATION = "";
+
+    public static String USER_STATUS_LOCK = "";
+
+    public static String USER_STATUS_NORMAL = "";
+
+    public static String SUPER_ADMIN_USERNAME = "admin";
+
+    public static Integer MEMBER_OPEN = 1;
+
+    public static Integer MEMBER_FORBIDDEN = 0;
+    public final static Integer APPLYING = 1;
+
+    public final static Integer APPROVAL = 2;
+
+    public final static Integer REJECTED = 3;
+
+    public final static String PAYING_SUCCESS = "success";
+
+    public final static String PAYING_ERROR = "error";
+
+    public final static String PAYING_PLANNING = "paying";
+
+    public final static String SEND_GIFT_RECORD = "give_gift";
+
+    public final static String RECEIVE_GIFT_RECORD = "exchange";
+
+    public final static String WITHDRAW_RECORD = "withdraw";
+
+    public final static String RECHARGE_RECORD= "recharge";
+
+    public final static String BUY_HAMMER_RECORD_= "buy_hammer";
+
+    //提现管理
+
+    public final static double MONEY_TO_CREDIT = 10;
+
+    public final static double CREDIT_TO_POINT = 0.6;
+
+    public final static double POINT_TO_CREDIT= 1;
+
+    public final static double POINT_TO_MONEY = 0.1;
+
+    public final static double CREDIT_TO_HAMMER = 0.05;
+
+    public final static double CREDIT_TO_POINT_ENTERTAINMENT = 0.8;
+}
