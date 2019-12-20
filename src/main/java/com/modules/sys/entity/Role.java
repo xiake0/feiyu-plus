a185c6a0d59681a4cb4de684d64f4be6729a2664
Route-l <15813384515@163.com>
2019-12-20 23:17:27 +0800

    update

---
+++ src/main/java/com/modules/sys/entity/Role.java
@@ -0,0 +1,11 @@
+package com.modules.sys.entity;
+
+import com.base.BaseEntity;
+
+/**
+ * @Author: xiake
+ * @Date: 2019/12/20 23:02
+ * @Description:
+ **/
+public class Role extends BaseEntity{
+}
