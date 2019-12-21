0c641066830faa1f12c2929f27ed931b92b94f83
Route-l <15813384515@163.com>
2019-12-21 22:58:12 +0800

    update

---
+++ src/main/java/com/modules/sys/entity/UserRole.java
@@ -0,0 +1,13 @@
+package com.modules.sys.entity;
+
+import com.base.BaseEntity;
+import lombok.Data;
+
+/**
+ * @Author: xiake
+ * @Date: 2019/12/21 22:57
+ * @Description:
+ **/
+@Data
+public class UserRole extends BaseEntity{
+}
